84e33c2c704c188fe38fd50ef392cc09380082b1
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-09-25 20:23:43 -0300

    Explaining what middleware dose

--- src/core.clj
+++ src/core.clj
@@ -4,58 +4,54 @@
    [reitit.ring :as ring]
    [ring.util.response :as resp]
    [ring.adapter.jetty :as jetty]
-  ;;  [clojure.java.jdbc :as j]
-  ;;  [honey.sql :as sql]
-  ;;  [clojure.spec.alpha :as s]
-  ;;  [reitit.coercion.spec]
-  ;;  [reitit.ring.coercion :as coercion]
-  ;;  [reitit.dev.pretty :as pretty]
-  ;;  [reitit.ring.middleware.muuntaja :as muuntaja]
-  ;;  [reitit.ring.middleware.exception :as exception]
-  ;;  [reitit.ring.middleware.multipart :as multipart]
-  ;;  [reitit.ring.middleware.parameters :as parameters]
-  ;;  [muuntaja.core :as m]
-  ;;  [clojure.string :as str]
-  ;;  [clojure.pprint :as pprint]
-  ;;  [clojure.string :as string]
-            ;; [user :as u]
-
+   [clojure.java.jdbc :as j]
+   [honey.sql :as sql]
+   [clojure.spec.alpha :as s]
+   [reitit.coercion.spec]
+   [reitit.dev.pretty :as pretty]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.exception :as exception]
+   [reitit.ring.middleware.parameters :as parameters]
+   [muuntaja.core :as m]
+   [ragtime.jdbc :as jdbc]
+   [core :as core]
+   [ragtime.repl :as rag]
    [clojure.pprint :as pprint])
-  ;; (:import [java.sql Date])
-  )
+  (:import [java.sql Date]))
 
 
-;; (def router-config
-;;   {:exception pretty/exception
-;;    :data {:coercion reitit.coercion.spec/coercion
-;;           :muuntaja m/instance
-;;           :middleware [;;  parameters/parameters-middleware
-;;                       ;;  muuntaja/format-negotiate-middleware
-;;                       ;;  muuntaja/format-response-middleware
-;;                       ;;  (exception/create-exception-middleware
-;;                       ;;   {::exception/default (partial exception/wrap-log-to-console
-;;                       ;;                                 exception/default-handler)})
-;;                       ;;  muuntaja/format-request-middleware
-;;                       ;;  coercion/coerce-response-middleware
-;;                       ;;  coercion/coerce-request-middleware
-;;                       ;;  multipart/multipart-middleware
-;;                        ]}})
+;; Database
 
-;; (def pg-db {:dbtype "postgresql"
-;;             :port 5432
-;;             :user "postgres"
-;;             :dbname "postgres"
-;;             :password "postgres"})
+(def pg-db {:dbtype "postgresql"
+            :port 5432
+            :user "postgres"
+            :dbname "postgres"
+            :password "postgres"})
 
-;; (defn query [q]
-;;   (->> q
-;;        (sql/format)
-;;        (j/query pg-db)))
+(defn query [q]
+  (->> q
+       (sql/format)
+       (j/query pg-db)))
 
-;; (defn insert [q]
-;;   (-> pg-db
-;;       (j/execute! (sql/format q) {:return-keys ["id"]})
-;;       (:generated_key)))
+(defn insert [q]
+  (-> pg-db
+      (j/execute! (sql/format q) {:return-keys ["id"]})
+      (:generated_key)))
+
+;; Migrations
+
+(def datastore (jdbc/sql-database core/pg-db))
+
+(defn config []
+  {:datastore  datastore
+   :migrations (jdbc/load-directory "./migrations")})
+
+(defn migrate []
+  (rag/migrate (config)))
+
+(defn rollback []
+  (rag/rollback (config)))
+
 
 ;; (defn max-n-characters [str n]
 ;;   (>= n (count str)))
@@ -86,17 +82,36 @@
 ;; (defn handler [_]
 ;;   (resp/created ""))
 
+;; Router
+
+(def router-config
+  {:exception pretty/exception
+   :data {:coercion reitit.coercion.spec/coercion
+          :muuntaja m/instance
+          :middleware [;; Put :query-params in the request, otherwise is just :query-string
+                       parameters/parameters-middleware
+                       ;; Not required for the example since we don't return bodies
+                       ;;  muuntaja/format-response-middleware
+                       ;; Required to parse body into :body-params
+                       muuntaja/format-request-middleware
+                       ;; Put :body-params in the request, otherwise is just :body
+                       muuntaja/format-negotiate-middleware
+                       (exception/create-exception-middleware
+                        {::exception/default (partial exception/wrap-log-to-console
+                                                      exception/default-handler)})]}})
+
 (def app (ring/ring-handler
-          (ring/router [["/pessoas" {:post (fn [_] (resp/created "/pessoas/1"))
+          (ring/router [["/pessoas" {:post (fn [_req]
+                                            ;;  (clojure.pprint/pprint req)
+                                             (resp/created "/pessoas/1"))
                                      :get (fn [_] (resp/status 200))}]
                         ["/pessoas/:id" {:get (fn [_] (resp/status 200))}]
                         ["/contagem-pessoas" {:get (fn [_] (resp/status 200))}]]
-                      ;;  router-config
-                       )))
+                       router-config)))
 
 (defn start []
   (println "Jetty is starting...")
-  (jetty/run-jetty #'app {:port 8080, :join? false})
+  (jetty/run-jetty #'app {:port 8081, :join? false})
   (println "Jetty is running..."))
 
 (defn -main []
