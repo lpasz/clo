659447484ba62c99ba7805d58aa7199b692d15a6
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2023-09-28 10:58:22 -0300

    Single file example

--- src/core.clj
+++ src/core.clj
@@ -3,36 +3,33 @@
   (:require [clojure.java.jdbc :as j]
             [clojure.spec.alpha :as s]
             [clojure.string :as str]
-            [clojure.core.async :as a]
-            [clojure.spec.gen.alpha :as gen]
+            [hikari-cp.core :as hcp]
             [honey.sql :as sql]
             [muuntaja.core :as m]
             [ragtime.jdbc :as jdbc]
             [ragtime.repl :as rag]
+            [ragtime.strategy :as rs]
             [reitit.coercion.spec]
             [reitit.dev.pretty :as pretty]
             [reitit.ring :as ring]
-            [ragtime.strategy :as rs]
             [reitit.ring.middleware.exception :as exception]
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.parameters :as parameters]
             [ring.adapter.jetty :as jetty]
-            [hikari-cp.core :as hcp]
-            [ring.util.response :as resp]
-            [clojure.pprint :as pp])
+            [ring.util.response :as resp])
   (:import [java.sql Date]))
 
-;; Database
+;; Database Config with Hikari connection pool
 
 (def postgres-url (or (System/getenv "POSTGRES_URL")
-                      "postgres://postgres:postgres@localhost:5432/postgres"))
+                      "postgres://clo_user:clo_password@localhost:5432/clo_db"))
 
 (def db-uri (java.net.URI. postgres-url))
 
 (defn datasource-options []
   (let [[username password] (str/split (or (.getUserInfo db-uri) ":") #":")]
-    {:username           (or username "postgres")
-     :password           (or password "postgres")
+    {:username           username
+     :password           password
      :port-number        (.getPort db-uri)
      :database-name      (str/replace-first (.getPath db-uri) "/" "")
      :server-name        (.getHost db-uri)
@@ -51,6 +48,8 @@
 (def db-conn
   (delay {:datasource (hcp/make-datasource (datasource-options))}))
 
+;; Database query/insert functions with honeysql
+
 (defn query [q]
   (->> q
        (sql/format)
@@ -64,40 +63,21 @@
       (j/execute! (sql/format q) {:return-keys ["id"]})
       (:id)))
 
-;; Migrations
-
-(defn config []
-  {:datastore  (jdbc/sql-database postgres-url)
-   :migrations (jdbc/load-directory "./migrations")
-   :strategy rs/rebase})
-
-(defn migrate []
-  (rag/migrate (config)))
-
-(defn rollback []
-  (rag/rollback (config)))
-
 ;; Context 
 
 (defn max-n-characters [str n]
   (>= n (count str)))
 
-(defn min-n-characters [str n]
-  (<= n (count str)))
 
-(s/def ::nome (s/and string? #(min-n-characters % 10) #(max-n-characters % 100)))
-(s/def ::apelido (s/and string? #(min-n-characters % 10) #(max-n-characters % 32)))
-(s/def ::stack (s/coll-of (s/and string? #(min-n-characters % 10) #(max-n-characters % 32))))
-(s/def ::nascimento #{"2016-11-18" "2015-7-19" "1987-11-16" "2011-1-12"
-                      "1984-3-21" "1989-6-9" "1989-8-1" "2006-8-5" "2011-10-13"})
-(s/def ::pessoas (s/keys :req-un [::nome ::apelido ::nascimento ::stack]))
+;; spec to check stack
+(s/def ::stack (s/or :nil nil? :stack-coll (s/coll-of (s/and string? #(max-n-characters % 32)))))
 
 (defn uuid [] (java.util.UUID/randomUUID))
 
 (defn parse-stack [{:keys [stack]}]
   (if (s/valid? ::stack stack)
     (->> stack
-         (filter #(not (nil? %)))
+         (filter not-empty)
          (str/join ";"))
     (throw (Exception. "Invalid Stack"))))
 
@@ -120,12 +100,10 @@
              :values [data]})))
 
 (defn pessoa-by-search-term [term]
-  (let [id (uuid)]
-    (-> {:select [:id :apelido :nome :nascimento :stack]
-         :from :pessoas
-         :where [:ilike :search (str "%" term "%")]}
-        (query))))
-
+  (-> {:select [:id :apelido :nome :nascimento :stack]
+       :from :pessoas
+       :where [:ilike :search (str "%" term "%")]}
+      (query)))
 
 (defn pessoa-by-id [id]
   (->> {:select [:id :apelido :nome :nascimento :stack]
@@ -193,13 +171,30 @@
 
 (def server-port (Integer/parseInt (or (System/getenv "SERVER_PORT") "8080")))
 
+(def jetty-server (delay (jetty/run-jetty #'app {:port server-port, :join? false})))
+
 (defn start []
   (println (str "Jetty is starting in " server-port "..."))
-  (jetty/run-jetty #'app {:port server-port, :join? false})
+  @jetty-server
   (println (str "Jetty is running on " server-port "...")))
 
-(defn -main []
+;; Migrations
+
+(defn config []
+  {:datastore  (jdbc/sql-database postgres-url)
+   :migrations (jdbc/load-directory "./migrations")
+   :strategy rs/rebase})
+
+(defn migrate []
   (try
-    (migrate)
-    (catch Exception e nil))
+    (rag/migrate (config))
+    (catch Exception _ nil)))
+
+(defn rollback []
+  (rag/rollback (config)))
+
+(defn -main []
+  ;; Done in a single connection before we start the connection pool
+  (migrate)
+  ;; Start web server
   (start))
